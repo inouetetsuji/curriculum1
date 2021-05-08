b6f20b731c65602c8e9c6be22708c5085157294a
inouetetsuji <motimawashimaeotoshi228@gmail.com>
2021-05-08 16:11:36 +0900

    課題提出1_3
    JAVA第一章「演算子」

---
+++ Task1_3.java
@@ -0,0 +1,21 @@
+public class Task1_3{
+    public static void main(String[]args){
+      int plus=0;
+      plus=5+10;
+      int minus=20;
+      minus-=7;
+      //int型の変数multiplyを宣言し、10に2を乗じた値を初期値とする。
+      int multiplay=10*2;
+      int remi=20;
+      remi%=6;
+      //文字列「town」を宣言し、初期値を「有楽町」とする。文字列「line」を宣言し、初期値を「線」とする。
+      String town="有楽町";
+      String line="線";
+      String train=town+line;
+      System.out.println(plus);
+      System.out.println(minus);
+      System.out.println(multiplay);
+      System.out.println(remi);
+      System.out.println(train);
+    }
+}
