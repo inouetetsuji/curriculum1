3b0792da0bda6fc9c7e57103773c9ea1d12b10ee
inouetetsuji <motimawashimaeotoshi228@gmail.com>
2021-06-22 10:45:37 +0900

    JAVA 第二章　メソッド 修正後エラー
    JAVA第二章　メソッド　
    修正後エラーです。

--- Main.java
+++ Main.java
@@ -7,19 +7,17 @@ public class Main {
         System.out.println(getName(firstName,lastName));           
         System.out.println(); 
      int[]arr= {11,12,13,14,15,16,17,18,19,20}; {
-     for(int num=10;num<arr.length+10;num++){
-       isOdd(num); 
+     for(int num=10;num<=arr.length+10;num++){
+    	 isOdd(arr[num]);
      }
      }
 	}
         public static String getName (String firstName, String lastName) {
-	    String first_name=firstName;
-	    String last_name=lastName;
-    	return  first_name+last_name;
+    	return  firstName+lastName;
         }
-        public static void isOdd(int num) {
-          if(num % 2!=0) {
-          System.out.println(num+"は奇数です。");
+        public static void isOdd(int arr[]) {
+          if(arr[num]% 2!=0) ;{
+          System.out.println(arr[num]+"は奇数です。");
           }
         }
  }
