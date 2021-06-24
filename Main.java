2916d6def805dbbb529a8138d1655737e407e3d8
inouetetsuji <motimawashimaeotoshi228@gmail.com>
2021-06-24 18:01:54 +0900

    java 第二章　メソッド　課題提出
    java 第二章　メソッドの課題提出

--- Main.java
+++ Main.java
@@ -6,18 +6,17 @@ public class Main {
     final String lastName="inoue";
         System.out.println(getName(firstName,lastName));           
         System.out.println(); 
-     int[]arr= {11,12,13,14,15,16,17,18,19,20}; {
-     for(int num=10;num<=arr.length+10;num++){
-    	 isOdd(arr[num]);
+     int[]arr= {11,12,13,14,15,16,17,18,19,20}; 
+    	 for(int n=10;n<=arr.length+10;n++){
+    		 isOdd(n);
+    	 }
      }
-     }
-	}
         public static String getName (String firstName, String lastName) {
     	return  firstName+lastName;
-        }
-        public static void isOdd(int arr[]) {
-          if(arr[num]% 2!=0) ;{
-          System.out.println(arr[num]+"は奇数です。");
+        }      
+        public static void isOdd(int num) {
+          if(num% 2!=0) {
+          System.out.println(num+"は奇数です。");
           }
         }
- }
+}
