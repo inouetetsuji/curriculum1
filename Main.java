29b57bbbe78849a0bb40b80d1eeb5f44d874c234
inouetetsuji <motimawashimaeotoshi228@gmail.com>
2021-06-29 17:58:49 +0900

    JAVA第二章メソッド　6/29作成
    JAVA 第二章　メソッド　6/29作成分です。

--- Main.java
+++ Main.java
@@ -7,8 +7,8 @@ public class Main {
         System.out.println(getName(firstName,lastName));           
         System.out.println(); 
      int[]arr= {11,12,13,14,15,16,17,18,19,20}; 
-    	 for(int n=10;n<=arr.length+10;n++){
-    		 isOdd(n);
+    	 for(int n=0;n<arr.length;n++){
+    		 isOdd(arr[n]);
     	 }
      }
         public static String getName (String firstName, String lastName) {
